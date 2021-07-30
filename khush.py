81d39a06f26766a8cb150cfeddcd4777ea69cc37
Khush Jammu <khushjammu@gmail.com>
2021-07-31 00:38:41 +0800

    changes

--- khush.py
+++ khush.py
@@ -48,8 +48,8 @@ from rljax.env import make_atari_env
 from rljax.algorithm import FQF
 from rljax.trainer import Trainer
 
-NUM_AGENT_STEPS = 3 * 10**5 # 20000
-EVAL_INTERVAL = 5000 # 1000
+NUM_AGENT_STEPS = 300000 # 20000
+EVAL_INTERVAL = 50000 # 5000 # 1000
 SEED = 0
 
 # env = gym.make("CartPole-v0")
@@ -72,22 +72,42 @@ env_test = make_atari_env("BreakoutNoFrameskip-v4", episode_life=False, sign_rew
 #     lr=1e-3,
 # )
 
+# algo = FQF(
+#         num_agent_steps=NUM_AGENT_STEPS,
+#         state_space=env.observation_space,
+#         action_space=env.action_space,
+#         seed=SEED,
+#         max_grad_norm=None,
+#         gamma=0.99,
+#         nstep=1,
+#         buffer_size=10 ** 6,
+#         use_per=False,
+#         batch_size=32,
+#         start_steps=50000,
+#         update_interval=4,
+#         update_interval_target=10000, # changed to fit pytorch one
+#         eps=0.01,
+#         eps_eval=0.001,
+#         eps_decay_steps=250000,
+#         loss_type="huber",
+#         dueling_net=False,
+#         double_q=False,
+#         setup_net=True,
+#         fn=None,
+#         lr=5e-5,
+#         lr_cum_p=2.5e-9,
+#         units=(512,),
+#         num_quantiles=32,
+#         num_cosines=64,
+#     )
+
 algo = FQF(
-    num_agent_steps=100000,
-    state_space=env.observation_space,
-    action_space=env.action_space,
-    seed=SEED,
-    batch_size=256,
-    start_steps=1000,
-    update_interval=1,
-    update_interval_target=400,
-    eps_decay_steps=0,
-    loss_type="l2",
-    lr=1e-3,
-    use_per=True,
-    dueling_net=True,
-    double_q=True,
-)
+        num_agent_steps=NUM_AGENT_STEPS,
+        state_space=env.observation_space,
+        action_space=env.action_space,
+        seed=SEED,
+        update_interval_target=10000, # changed to fit pytorch one
+    )
 
 trainer = Trainer(
     env=env,
